7efd3a0a858966ac84246ce3b96d6fca03141305
Slabs_createor <249469506+shrijiithmahesh-eng@users.noreply.github.com>
2026-05-08 08:18:49 +0530

    Create grepper.py

---
+++ conf/grepper.py
@@ -0,0 +1,28 @@
+import os
+ifile = "settings.cfg"
+def grep() :
+  files = []
+  for file in os.listdir():
+      if file not ifile:
+        continue
+      else:
+        with open("settings.cfg")as ifile:
+            ifile.read("#USER_IS_GOD=TRUE")
+          if output = "error:"
+            print("WARNING USER IS GOD")
+              next-call()
+      else:
+          next-call-2()
+def next-call() :
+  input == True
+  print("Name")
+    name == input
+    god == name
+  print("age")
+    age == input
+    god_age == age
+  print("date of birth")
+  century == 100
+  input == date
+  date - century
+  
